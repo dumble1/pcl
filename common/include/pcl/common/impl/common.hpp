5c33527f75dd80edd36881be7e4773df4111ede2
Seungsu Shin <dumble140@snatcher.ai>
2019-03-06 10:42:09 +0900

    using iterator

--- common/include/pcl/common/impl/common.hpp
+++ common/include/pcl/common/impl/common.hpp
@@ -285,13 +285,13 @@ pcl::getMinMax3D (const pcl::PointCloud<PointT> &cloud, Eigen::Vector4f &min_pt,
   // If the data is dense, we don't need to check for NaN
   if (cloud.is_dense)
   {
-    for (typename pcl::PointCloud<PointT>::size_type i = 0; i < cloud.points.size(); ++i)
+    for (typename pcl::PointCloud<PointT>::iterator i = cloud.begin(); i != cloud.end(); ++i)
     {
-      pcl::Array4fMapConst pt = cloud.points[i].getArray4fMap ();
+      pcl::Array4fMapConst pt = (*i).getArray4fMap ();
       min_p = min_p.min (pt);
       max_p = max_p.max (pt);
       if(pt[2]>200)
-        printf("this is the max 3D value : %f %f %f\n and %zu th pt : %f %f %f\n", max_p[0], max_p[1],max_p[2],i ,pt[0],pt[1],pt[2]);
+        printf("this is the max 3D value : %f %f %f\n and  th pt : %f %f %f\n", max_p[0], max_p[1],max_p[2] ,pt[0],pt[1],pt[2]);
     }
   }
   // NaN or Inf values could exist => check for them
