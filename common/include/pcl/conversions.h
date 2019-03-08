709adbfc3fefbd42a89b9d10490958b2920574d2
Seungsu Shin <dumble140@snatcher.ai>
2019-03-08 13:19:49 +0900

    wip

--- common/include/pcl/conversions.h
+++ common/include/pcl/conversions.h
@@ -173,12 +173,6 @@ namespace pcl
     cloud.width    = msg.width;
     cloud.height   = msg.height;
     cloud.is_dense = msg.is_dense == 1;
-
-    ofstream writeFile("data.txt");
-    if(writeFile.is_open()){
-      writeFile << msg << "\n";
-      writeFile.close();
-    }
     // Copy point data
     uint32_t num_points = msg.width * msg.height;
     cloud.points.resize (num_points);
